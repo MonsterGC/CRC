2140be8a4bde753a3a5914e9e0b350cac63e5c81
GCMonster <1724264854@qq.com>
2020-10-19 15:02:27 +0800

    Update

--- JS/testErr.js
+++ JS/testErr.js
@@ -3,12 +3,14 @@ const keys = require('./utils/keys')
 
 // 发送过程中错误检测
 utils.readFile('./JS/output.txt').then((M) => {
-    console.log(`发送错误前M:${M}`)
+    // console.log(`发送错误前M:${M}`)
+    
+    
     // 模拟发送过程中存在错误(第一位数据错误)
     tmp = M.split("")
     tmp[0] = tmp[0] == '0' ? '1' : '0'
     M = tmp.join("")
-    console.log(`发送错误后M:${M}`)
+    // console.log(`发送错误后M:${M}`)
 
     var R = utils.auth(M, keys.P)
     console.log(`(错误)检验出来的余数${R}`)
